e231b2a2fd3f44dde4c541cb332306ef1405bd64
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-07-20 15:48:53 +0700

    create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,56 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
+    puts @article
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    @article = Article.new(params_article)
+    if @article.save
+        flash[:notice] = "Success Add Records"
+        redirect_to action: 'index'
+    else
+        flash[:error] = "data not valid"
+        render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params_article)
+        flash[:notice] = "Success Update Records"
+        redirect_to action: 'index'
+    else
+        flash[:error] = "data not valid"
+        render 'edit'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+        flash[:notice] = "Success Delete a Record"
+        redirect_to action: 'index'
+    else
+        flash[:error] = "Fails Delete a Record"
+        redirect_to action: 'index'
+    end
+  end
+
+  private
+    def params_article
+        params.require(:article).permit(:title, :content, :status, :picture)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720084023) do
+ActiveRecord::Schema.define(version: 20160720084752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,13 @@ ActiveRecord::Schema.define(version: 20160720084023) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.string   "status"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
 end
