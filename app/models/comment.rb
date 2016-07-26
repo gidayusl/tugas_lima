257b848df0896382cbd2283114ffa1b4c8c21ff1
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-07-26 19:59:36 +0700

    add field comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ApplicationRecord
   #name relation must singular
+  before_create :default_status
   belongs_to :article
-  default_scope {where(status: 'active')}
+  validates :content, presence: true, length: { minimum: 10 }
+  
+  before_create :default_status
+  
+  def default_status
+      self.status = "off"
+  end
 end
