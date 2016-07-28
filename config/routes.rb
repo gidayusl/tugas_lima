77ddad00be483f3115bd24fd61851ea965a06f34
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-07-28 13:30:23 +0700

    modified for signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get "sign_up" => "users#new", :as => "sign_up"
-  resources :users
 
   resources :articles
   
   resources :comments
+  
+  get "sign_up" => "users#new", :as => "sign_up"
+  resources :users
+  
+  get "login" => "sessions#new", :as => "login"
+  post "create" => "sessions#create"
+  get "logout" => "sessions#logout", :as => "logout"
+  resources :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727032544) do
+ActiveRecord::Schema.define(version: 20160727051059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,9 @@ ActiveRecord::Schema.define(version: 20160727032544) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "activation_status"
   end
 
 end
